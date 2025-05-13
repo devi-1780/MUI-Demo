7e309e7d0fa2b6362a4f92a379d4ce50ba596fc1
Devi <devi131780@gmail.com>
2025-05-13 22:03:50 +0530

    adding ToggleButtonGrop and exclusive=radio button

--- src/components/MuiButton.jsx
+++ src/components/MuiButton.jsx
@@ -1,6 +1,23 @@
-import { Button, ButtonGroup, IconButton, Stack } from "@mui/material";
+import {
+  Button,
+  ButtonGroup,
+  IconButton,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+import { useState } from "react";
+
 function MuiButton() {
+  let [format, setFormat] = useState(null);
+  const handleToggleButton = (event, updatedFormat) => {
+    setFormat(updatedFormat);
+    console.log("selected format", updatedFormat);
+  };
   return (
     <div>
       <Stack spacing={4}>
@@ -67,6 +84,27 @@ function MuiButton() {
             <Button onClick={() => alert("center clicked")}>Center</Button>
             <Button onClick={() => alert("right clicked")}>Right</Button>
           </ButtonGroup>
+        </Stack>
+        <Stack direction={"row"}>
+          <ToggleButtonGroup
+            aria-label="text-formating"
+            orientation="vertical"
+            onChange={handleToggleButton}
+            value={format}
+            exclusive
+            color="success"
+            size="small"
+          >
+            <ToggleButton value={"bold"} aria-label="bold">
+              <FormatBoldIcon />
+            </ToggleButton>
+            <ToggleButton value={"italic"} aria-label="italic">
+              <FormatItalicIcon />
+            </ToggleButton>
+            <ToggleButton value={"underline"} aria-label="underline">
+              <FormatUnderlinedIcon />
+            </ToggleButton>
+          </ToggleButtonGroup>
         </Stack>
       </Stack>
     </div>
