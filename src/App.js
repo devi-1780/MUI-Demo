6928456131b689e6b2da4407e83e047047552019
Devi <devi131780@gmail.com>
2025-05-13 17:09:19 +0530

    added mui-button component with colors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { Link, Route, Routes } from "react-router";
 import "./App.css";
 
 import MuiTypography from "./components/MuiTypography";
+import { Button, Stack } from "@mui/material";
+import MuiButton from "./components/MuiButton";
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography />{" "}
+      <nav style={{ marginBottom: "15px" }}>
+        <Stack spacing={4} direction={"row"}>
+          <Link to="/">
+            <Button variant="contained">Mui-Typography</Button>
+          </Link>
+          <Link to="buttonComponent">
+            <Button variant="contained">Mui-Button</Button>
+          </Link>
+        </Stack>
+      </nav>
+      <Routes>
+        <Route path="/" element={<MuiTypography />} />
+        <Route path="/buttonComponent" element={<MuiButton />} />
+      </Routes>
     </div>
   );
 }
