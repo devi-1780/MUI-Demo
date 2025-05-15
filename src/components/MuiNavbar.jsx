02c38d7b547f2c63b495c0ef57f96bb6d2b62b6c
Devi <devi131780@gmail.com>
2025-05-15 14:16:23 +0530

    changes html nav to Navbar component

--- src/components/MuiNavbar.jsx
+++ src/components/MuiNavbar.jsx
@@ -1,14 +1,32 @@
-import { AppBar, Box, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
 function MuiNavbar() {
   return (
-    <Box>
-      <AppBar>
-        <Toolbar>
-          <CatchingPokemonIcon />
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" size="large" color="inherit" label="logo">
+          {" "}
+          <CatchingPokemonIcon color="white" />
+        </IconButton>
+        <Typography variant="h6" component={"div"} sx={{ flexGrow: 1 }}>
+          Pokemon app
+        </Typography>
+        <Stack direction={"row"} spacing={2}>
+          <Button color="inherit">Features</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Pricing</Button>
+          <Button color="inherit">login</Button>
+        </Stack>
+      </Toolbar>
+    </AppBar>
   );
 }
 export default MuiNavbar;
